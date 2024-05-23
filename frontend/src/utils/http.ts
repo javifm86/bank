bc5aa8e5d8d18c84d50ecc8cc5c00e28b9cb5835
CTS\2055384 <2055384@cognizant.com>
2024-05-23 12:26:14 +0200

    add token on each request for axios instance

--- frontend/src/utils/http.ts
+++ frontend/src/utils/http.ts
@@ -1,7 +1,21 @@
 import axios from 'axios';
 
+let token: string | null = null;
+
 const http = axios.create({
   baseURL: 'http://localhost:3001',
 });
+
+http.interceptors.request.use(
+  (config) => {
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default http;
