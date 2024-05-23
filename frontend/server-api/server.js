50509f2eaa29bc5af13c9dfa4c68f5dd2606d0b7
CTS\2055384 <2055384@cognizant.com>
2024-05-23 11:39:25 +0200

    add json server for mocks and axios

---
+++ frontend/server-api/server.js
@@ -0,0 +1,28 @@
+import jsonServer from 'json-server';
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+server.post('/login', function (req, res, next) {
+  req.method = 'GET';
+  req.query = req.body;
+
+  const { userName, password } = req.query;
+
+  if (userName === 'admin' && password === 'admin') {
+    res.status(200).send({
+      token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+    });
+  } else {
+    res.status(401).send({});
+  }
+});
+
+server.use(router);
+server.listen(3001, function () {
+  console.log('JSON Server is running');
+});
