4c0593d152724f031dc28f93d5c382ec81e65609
CTS\2055384 <2055384@cognizant.com>
2024-05-23 11:40:29 +0200

    add post login

---
+++ frontend/src/api/login.ts
@@ -0,0 +1,27 @@
+import http from '../utils/http';
+
+interface PostLoginParams {
+  userName: string;
+  password: string;
+}
+
+interface PostLoginResponse {
+  token: string;
+}
+
+async function postLogin(params: PostLoginParams) {
+  try {
+    const { data } = await http.post<PostLoginResponse>('/login', params);
+    return {
+      error: false,
+      data,
+    };
+  } catch (error) {
+    return {
+      error: true,
+      data: null,
+    };
+  }
+}
+
+export default postLogin;
