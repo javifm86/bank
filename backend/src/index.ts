48adebcc69ead1a60219bdc0d58caf028df7c98a
javifm86 <javifm86@gmail.com>
2024-05-26 22:44:02 +0200

    add post movement

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,14 @@ app.use(express.json());
 
 app.use(cors({ origin: 'http://localhost:5173' }));
 
+interface CustomJwtPayload extends JwtPayload {
+  username: string;
+}
+
+interface RequestWithUser extends Request {
+  user?: string | JwtPayload | undefined;
+}
+
 interface User {
   username: string;
   password_hash: string;
@@ -25,10 +33,22 @@ interface Movement {
   balance: number;
 }
 
+interface Account {
+  username: string;
+  balance: number;
+}
+
 interface LoginPostRequest extends Request {
   body: {
     username: string;
     password: string;
+  };
+}
+
+interface PostMovementRequest extends RequestWithUser {
+  body: {
+    amount?: number;
+    type?: 'withdraw' | 'deposit';
   };
 }
 
@@ -89,13 +109,61 @@ app.get('/movements', (req: RequestWithUser, res) => {
     });
 });
 
-interface CustomJwtPayload extends JwtPayload {
-  username: string;
-}
+app.post('/movements', (req: PostMovementRequest, res) => {
+  const { amount, type } = req.body;
 
-interface RequestWithUser extends Request {
-  user?: string | JwtPayload | undefined;
-}
+  if (
+    !amount ||
+    !type ||
+    !Number.isInteger(amount) ||
+    (type !== 'withdraw' && type !== 'deposit')
+  ) {
+    res.status(400).json({ error: 'Error validating params' });
+    return;
+  }
+
+  db.any<Account>('SELECT balance FROM account WHERE username = $1', [req.user])
+    .then((results) => {
+      if (results.length) {
+        const balance = results[0].balance;
+        const newBalance =
+          type === 'deposit' ? balance + amount : balance - amount;
+
+        if (newBalance < 0) {
+          res.status(400).json({ error: 'Not enough balance' });
+          return;
+        }
+
+        db.tx(async (t) => {
+          const queryMovement = t.none(
+            'INSERT INTO movements (username, date, type, amount, balance) VALUES ($1, NOW(), $2, $3, $4)',
+            [req.user, type, amount, newBalance]
+          );
+
+          const queryBalance = t.none(
+            'UPDATE account SET balance = $1 WHERE username = $2',
+            [newBalance, req.user]
+          );
+
+          await t.batch([queryMovement, queryBalance]);
+        })
+          .then(() => {
+            res.json({ message: 'Movement created' });
+          })
+          .catch((error) => {
+            console.log(error);
+            res.status(400).json({ error: 'Error creating movement' });
+          });
+      } else {
+        res.status(400).json({
+          error: 'No results obtained from db.',
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(400).json({ error: 'Generic error in movements' });
+    });
+});
 
 function verifyToken(req: RequestWithUser, res: Response, next: NextFunction) {
   // Exclude /login route from JWT validation
