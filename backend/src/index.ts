b10e86bf5e6d837c5109837d9116acd353d502ba
javifm86 <javifm86@gmail.com>
2024-05-27 10:51:33 +0200

    add posibility to specify port in env file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv';
 import app from './app';
 
-const port = process.env.PORT || '3000';
+dotenv.config();
+
+const port = process.env.PORT ?? '3000';
 app.listen(port, () => console.log(`Server started on port ${port}`));
