18c1157fc6365a96b7e400b5bb67156858def672
javifm86 <javifm86@gmail.com>
2025-05-22 13:07:39 +0200

    populate env variables only for development

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import dotenv from 'dotenv';
 import app from './app';
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 
 const port = process.env.BACKEND_PORT ?? '3000';
 app.listen(port, () => console.log(`Server started on port ${port}`));
