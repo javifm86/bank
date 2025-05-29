21f84168a7a3d76ae56d5201e60dc8515aaa12df
javifm86 <javifm86@gmail.com>
2025-05-29 13:38:39 +0200

    add FRONTEND_DOMAIN for backend environment

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,10 +4,11 @@ import login from './controllers/login';
 import { getMovements, postMovements } from './controllers/movements';
 import { verifyToken } from './middleware/auth';
 
+const FRONTEND_DOMAIN = process.env.FRONTEND_DOMAIN ?? 'http://localhost:5173';
 const app = express();
 app.use(express.json());
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: FRONTEND_DOMAIN }));
 
 app.use(verifyToken);
 app.post('/login', login);
